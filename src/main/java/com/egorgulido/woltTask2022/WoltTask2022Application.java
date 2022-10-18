332ad7addf673b869924d6618a3874da01d99f8b
Egor-oop <gulidoegor@gmail.com>
2022-10-18 19:46:08 +0300

    Initial commit

---
+++ src/main/java/com/egorgulido/woltTask2022/WoltTask2022Application.java
@@ -0,0 +1,13 @@
+package com.egorgulido.woltTask2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoltTask2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoltTask2022Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/egorgulido/woltTask2022/WoltTask2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egorgulido.woltTask2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoltTask2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

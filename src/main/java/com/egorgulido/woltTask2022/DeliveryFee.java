0038cb151f07ed9c1dcac1fc1fbb0be4a462bb72
Egor-oop <gulidoegor@gmail.com>
2022-10-18 21:12:28 +0300

    feat: fee

---
+++ src/main/java/com/egorgulido/woltTask2022/DeliveryFee.java
@@ -0,0 +1,18 @@
+package com.egorgulido.woltTask2022;
+
+public class DeliveryFee {
+
+    public int fee;
+
+    public DeliveryFee(int fee) {
+        this.fee = fee;
+    }
+
+    public void setFee(int fee) {
+        this.fee = fee;
+    }
+
+    public int getFee() {
+        return fee;
+    }
+}
